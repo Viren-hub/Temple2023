e72afee1ae2309d3d85dc1350123ea495b3d41a2
Manojpatil0782 <122069469+Manojpatil0782@users.noreply.github.com>
2023-02-06 01:05:35 +0530

    Footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Footer.css'
-
+import { FaFacebook,FaInstagram,FaTwitter} from "react-icons/fa";
+import temple from '../../assets/Footer1.jpg'
 export default function Footer() {
   return (
     <div className='footer'>
-        <h2>Footer</h2>
+        <div className='f1'>
+          <img src={temple} alt="img"/>
+        </div>
+        <div className='f2'>
+           <h5><a href='#'>Link1</a></h5>
+          <h5><a href='#'>Link2</a></h5>
+          <h5><a href='#'>Link3</a></h5>
+        </div>
+        <div className='f3'>
+          <h2>CONNECT WITH US</h2>
+          <h2><a href=''><FaInstagram/></a></h2>
+          <h2><a href=''><FaFacebook/> </a></h2>
+          <h2><a href=''><FaTwitter/>  </a></h2>
+        </div>
     </div>
   )
 }
